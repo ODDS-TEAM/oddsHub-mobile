826e09a16a2939337ca6e5d6ea23ca042a5ae350
Pakornpat Sinjiranon <skyfox.ku@gmail.com>
2023-02-07 14:20:30 +0700

    create simple scrollview with course content

--- lib/home.dart
+++ lib/home.dart
@@ -7,14 +7,60 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
+class Course {
+  final String name;
+  final String description;
+  final String instructor;
+  final DateTime startDate;
+
+  Course(this.name, this.description, this.instructor, this.startDate);
+}
+
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    final List<Course> courses = [
+      Course('Scrum', 'Just overview', 'Jane', DateTime.now())
+    ];
+
     return Scaffold(
-      body: Center(
-        child: Text(
-          'headline 1',
-          style: Theme.of(context).textTheme.headline1,
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('OddsHub'),
+      ),
+      body: SingleChildScrollView(
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              for (var course in courses)
+                Container(
+                  margin: const EdgeInsets.all(10),
+                  padding: const EdgeInsets.all(10),
+                  height: 250,
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black26, width: 1),
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(30))),
+                  child: Center(
+                    child: Column(
+                      children: [
+                        Text(
+                          'Course Name: ${course.name}',
+                          style: Theme.of(context).textTheme.headline2,
+                        ),
+                        Text(
+                          'Description: ${course.description}',
+                          style: Theme.of(context).textTheme.headline2,
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+            ],
+          ),
         ),
       ),
     );
