3612b20d372778fa049cec483528561775468db2
Pakornpat Sinjiranon <skyfox.ku@gmail.com>
2023-02-08 16:37:00 +0700

    Adjust the ui registration success screen

--- lib/presentation/journey/course/course_list_constants.dart
+++ lib/presentation/journey/course/course_list_constants.dart
@@ -1,8 +1,9 @@
 class CourseListConstants {
   static const String oddsLogoImage = 'assets/images/logo.png';
   static const String productDiscoveryImage =
-      'assets/images/product_Dis_covery.png';
-  static const String productDiscoveryDescription = '''Product Discovery is a course that blends solid product management, user experience design, and design thinking into a collaborative team-based approach to product ownership.''';
+      'assets/images/product_discovery.png';
+  static const String productDiscoveryDescription =
+      '''Product Discovery is a course that blends solid product management, user experience design, and design thinking into a collaborative team-based approach to product ownership.''';
   static const String instructorName = 'Jean';
   static const String courseName = 'Product discovery';
 }
--- lib/presentation/journey/registration/registration_success/registration_success_screen.dart
+++ lib/presentation/journey/registration/registration_success/registration_success_screen.dart
@@ -15,42 +15,65 @@ class RegistrationSuccessScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final person = ModalRoute.of(context)?.settings.arguments as Person?;
     final name = person?.fullName ?? '';
-
-    return Container(
-      color: AppColors.primaryBackground,
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            Text(
-              key: RegistrationSuccessConstants.titleKey,
-              'registration complete',
-              style: Theme.of(context).textTheme.subtitle1,
+    return Scaffold(
+      backgroundColor: AppColors.primaryBackground,
+      body: SafeArea(
+        minimum: const EdgeInsets.all(10),
+        child: SingleChildScrollView(
+          child: Container(
+            margin: const EdgeInsets.all(8),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 30,
+                ),
+                Text(
+                  key: RegistrationSuccessConstants.titleKey,
+                  'Registration Complete',
+                  style: Theme.of(context).textTheme.headline1,
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  key: RegistrationSuccessConstants.subtitleKey,
+                  name,
+                  style: Theme.of(context).textTheme.subtitle1,
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  key: RegistrationSuccessConstants.paymentMethodTitleKey,
+                  'Payment method',
+                  style: Theme.of(context).textTheme.subtitle2,
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Image(
+                  key: RegistrationSuccessConstants.paymentQrImageKey,
+                  image: const AssetImage('assets/images/payment-qr.jpeg'),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  key: RegistrationSuccessConstants.paymentDescriptionKey,
+                  'Please send the payment slip to this email jane@odds.team',
+                  style: Theme.of(context).textTheme.subtitle1,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                ElevatedButton(
+                  key: RegistrationSuccessConstants.okButtonKey,
+                  onPressed: okButtonDidTap,
+                  child: const Text('OK'),
+                )
+              ],
             ),
-            Text(
-              key: RegistrationSuccessConstants.subtitleKey,
-              name,
-              style: Theme.of(context).textTheme.subtitle1,
-            ),
-            Text(
-              key: RegistrationSuccessConstants.paymentMethodTitleKey,
-              'Payment method',
-              style: Theme.of(context).textTheme.subtitle1,
-            ),
-            Image(
-              key: RegistrationSuccessConstants.paymentQrImageKey,
-              image: const AssetImage('assets/images/payment-qr.jpeg'),
-            ),
-            Text(
-              key: RegistrationSuccessConstants.paymentDescriptionKey,
-              'Please send the payment slip to this email jane@odds.team',
-              style: Theme.of(context).textTheme.subtitle1,
-            ),
-            ElevatedButton(
-              key: RegistrationSuccessConstants.okButtonKey,
-              onPressed: okButtonDidTap,
-              child: const Text('OK'),
-            )
-          ],
+          ),
         ),
       ),
     );
