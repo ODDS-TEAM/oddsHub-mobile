94586cf5c6f68b7e7bd34b419a66b8c62bc3c3b7
Pakornpat Sinjiranon <skyfox.ku@gmail.com>
2023-02-08 14:45:34 +0700

    Change tests structure

--- test/mock.dart
+++ test/mock.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:mocktail/mocktail.dart';
 
-
 class FakeRoute extends Fake implements Route {
   static void setUp() => registerFallbackValue(FakeRoute());
 }
+
 class MockNavigatorObserver extends Mock implements NavigatorObserver {
   static void setUp() {
     registerFallbackValue(FakeRoute());
