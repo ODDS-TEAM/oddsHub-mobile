ebca0faed5caa8139c07c4664a86cf0ec57e472a
Chalittaya Nimpattanawong <aux-chalittaya.n@tech.jago.com>
2023-02-08 15:15:47 +0700

    create unit test for cancel button on registration screen

--- test/presentation/journey/registration/registration_screen_test.dart
+++ test/presentation/journey/registration/registration_screen_test.dart
@@ -20,7 +20,7 @@ void main() {
   }
 
   testWidgets(
-      'when user pres save button '
+      'when user press save button '
       'then navigate to registration success screen',
       (WidgetTester tester) async {
     final widget = prepareWidget();
@@ -30,5 +30,17 @@ void main() {
 
     verify(() => mockNavigatorObserver.didPush(any(), any()));
     expect(saveButton, findsOneWidget);
+  });
+
+  testWidgets(
+      'when user press cancel button '
+      'then navigate to course list screen', (WidgetTester tester) async {
+    final widget = prepareWidget();
+    final cancelButton = find.byKey(const Key('cancel-button'));
+
+    await tester.pumpWidget(widget);
+
+    verify(() => mockNavigatorObserver.didPush(any(), any()));
+    expect(cancelButton, findsOneWidget);
   });
 }
