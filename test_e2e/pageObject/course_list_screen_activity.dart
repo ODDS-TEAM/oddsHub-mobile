c341cd2cb15d166b7006dc4d3a706adeac81625e
Pakornpat Sinjiranon <skyfox.ku@gmail.com>
2023-02-09 18:09:59 +0700

    Fix lint

--- test_e2e/pageObject/course_list_screen_activity.dart
+++ test_e2e/pageObject/course_list_screen_activity.dart
@@ -1,17 +1,17 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:oddshub/presentation/journey/Course/course_list_constants.dart';
+import 'package:oddshub/presentation/journey/course/course_list_constants.dart';
 
 class CourseListScreenActivity {
   final WidgetTester tester;
 
   CourseListScreenActivity(this.tester);
-  
+
   Future<void> clickRegisterButton() async {
     final registerButton = find.byKey(CourseListConstants.registerButtonKey);
     await tester.tap(registerButton);
     await tester.pumpAndSettle();
   }
-  
+
   void waitUntilTheScreenShowsTitleMessage(String title) {
     final finder = find.text(title);
     expect(finder, findsOneWidget);
