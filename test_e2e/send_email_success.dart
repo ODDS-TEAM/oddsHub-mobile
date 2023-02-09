b349f04ee2162b029a0edd73ad4ef6f19fa06693
Lee.k <nut@odds.team>
2023-02-09 17:55:56 +0700

    fix e2e test fail

--- test_e2e/send_email_success.dart
+++ test_e2e/send_email_success.dart
@@ -2,13 +2,14 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:oddshub/presentation/journey/course/course_list_constants.dart';
 import 'package:oddshub/presentation/journey/send_email/send_email_constants.dart';
 
-import 'pageObject/kw_common.dart';
+import 'pageObject/common_activity.dart';
 
 void main(List<String> args) {
-  suiteSetUp();
+  CommonActivity.suiteSetUp();
 
   testWidgets('Send Email Successfully', (tester) async {
-    await openApplication(tester, 'OddsHub');
+    final commonActivity = CommonActivity(tester);
+    await commonActivity.openApplication('OddsHub');
     final goToSendEmail = find.byKey(CourseListConstants.sendEmailButtonKey);
     await tester.tap(goToSendEmail);
     await tester.pumpAndSettle();
