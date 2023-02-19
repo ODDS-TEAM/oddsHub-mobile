4c7967f7a133314085be07fa698e470131679000
MuyonZ <pattaraponler@odds.team>
2023-02-20 00:15:49 +0700

    Pass function and label to BaseDetailScreen

--- lib/course_list/widgets/detail.dart
+++ lib/course_list/widgets/detail.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:oddshub/course_list/model.dart';
 import 'package:oddshub/styles/colors.dart';
@@ -8,21 +10,51 @@ import 'course_image.dart';
 
 class ClassDetailScreen extends BaseDetailScreen {
   static const String path = '/class-detail';
-  const ClassDetailScreen({super.key}) : super(title: 'Class Detail');
+  const ClassDetailScreen({super.key})
+      : super(
+          title: 'Class Detail',
+          actionLabel: 'Register',
+        );
+
+  @override
+  void onAction() {
+    log(
+      'Class Action',
+      name: 'Debug Log',
+    );
+  }
 }
 
 class CourseDetailScreen extends BaseDetailScreen {
   static const String path = '/course-detail';
-  const CourseDetailScreen({super.key}) : super(title: 'Course Detail');
+  const CourseDetailScreen({super.key})
+      : super(
+          title: 'Course Detail',
+          actionLabel: 'Add Waiting List',
+        );
+
+  @override
+  void onAction() {
+    log(
+      'Course Action',
+      name: 'Debug Log',
+    );
+  }
 }
 
 class BaseDetailScreen extends StatelessWidget {
   final String title;
-  const BaseDetailScreen({super.key, required this.title});
+  final double spaceBetween;
+  final String actionLabel;
+  const BaseDetailScreen({
+    super.key,
+    required this.title,
+    required this.actionLabel,
+    this.spaceBetween = 8,
+  });
 
   @override
   Widget build(BuildContext context) {
-    const double spaceBetween = 8;
     final Course course = ModalRoute.of(context)?.settings.arguments as Course;
     return Scaffold(
       appBar: AppBar(title: Text(title)),
@@ -61,20 +93,20 @@ class BaseDetailScreen extends StatelessWidget {
                               ),
                             ],
                           ),
-                          const SizedBox(height: spaceBetween),
+                          SizedBox(height: spaceBetween),
                           Text(
                             course.instructor,
                             style: AppTextStyles.bodySmall.copyWith(
                               color: AppColors.primaryColor.shade200,
                             ),
                           ),
-                          const SizedBox(height: spaceBetween),
+                          SizedBox(height: spaceBetween),
                           Row(
                             children: [
                               Row(
                                 children: [
                                   const Icon(Icons.watch_later_outlined),
-                                  const SizedBox(width: spaceBetween),
+                                  SizedBox(width: spaceBetween),
                                   Text(course.formattedDate),
                                 ],
                               ),
@@ -82,7 +114,7 @@ class BaseDetailScreen extends StatelessWidget {
                               Row(
                                 children: [
                                   const Icon(Icons.people),
-                                  const SizedBox(width: spaceBetween),
+                                  SizedBox(width: spaceBetween),
                                   Text(course.remainderPeople),
                                 ],
                               ),
@@ -94,7 +126,7 @@ class BaseDetailScreen extends StatelessWidget {
                             style: AppTextStyles.bodyLarge,
                           ),
                           const SizedBox(height: kSpaceBetweenItem),
-                          ActionButton(course: course)
+                          _buildActionButton(course, spaceBetween, context),
                         ],
                       ),
                     ),
@@ -107,6 +139,42 @@ class BaseDetailScreen extends StatelessWidget {
       ),
     );
   }
+
+  Column _buildActionButton(
+    Course course,
+    double spaceBetween,
+    BuildContext context,
+  ) {
+    return Column(
+      children: [
+        Text(
+          'Price ${course.price}',
+          style: AppTextStyles.bodyLarge.copyWith(
+            color: AppColors.primaryColor.shade200,
+          ),
+        ),
+        SizedBox(height: spaceBetween),
+        SizedBox(
+          height: kButtonHeight,
+          width: MediaQuery.of(context).size.width,
+          child: ElevatedButton(
+            onPressed: onAction,
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(kBorderRadius),
+              ),
+            ),
+            child: Text(
+              actionLabel,
+              style: AppTextStyles.labelLarge,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  void onAction() {}
 }
 
 class ActionButton extends StatelessWidget {
